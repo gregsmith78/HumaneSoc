27c1f65aa36556d285e007a462063777ae889e51
gregsmith78 <ga.smithr78@gmail.com>
2016-04-01 17:51:52 -0500

    initial commit for humane society

---
+++ HumaneSociety/HumaneSociety/Owner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    class Owner
+    {
+        string name;
+        int age;
+        bool hasMoney;
+        string profession;
+        double wallet;
+
+    }
+}
